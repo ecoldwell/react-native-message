5b9abc33cf5ed30527f9ef7cb2a0560daac957da
Nomads <nomads@barolo.vit.lan>
2021-10-21 16:52:59 -0400

    commit with the keyboard

---
+++ ios/Keyboard/KeyboardView.swift
@@ -0,0 +1,23 @@
+//
+//  KeyboardView.swift
+//  Keyboard
+//
+//  Created by Nomads on 2021-10-21.
+//
+
+import Foundation
+import SwiftUI
+import KeyboardKit
+
+/**
+ This view is the main view that is used by the extension by
+ calling `setup(with:)` in `KeyboardViewController`.
+ 
+ The view will switch over the current keyboard type and add
+ the correct keyboard view.
+ 
+ Set `addTextFieldAboveKeyboard` to true to add a text field
+ above the demo keyboard. This text field will automatically
+ take over as the main proxy instead of the main app.
+ */
+
